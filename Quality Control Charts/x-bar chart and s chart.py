204e128ab2ae7637821a8aa3d60f5609969ed6a2
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-09 16:28:23 -0400

    Create x-bar chart and s chart.py

---
+++ Quality Control Charts/x-bar chart and s chart.py
@@ -0,0 +1,78 @@
+
+### x-bar chart and R chart ###
+
+# Import required libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statistics
+
+# Set random seed
+np.random.seed(42)
+
+# Set matplotlib style
+plt.style.use('ggplot')
+
+# Create dummy data
+x = np.array([list(np.random.normal(loc=10, scale=2, size=11)),
+        list(np.random.normal(loc=10, scale=2, size=11)),
+        list(np.random.normal(loc=10, scale=2, size=11)),
+        list(np.random.normal(loc=10, scale=2, size=11)),
+        list(np.random.normal(loc=10, scale=2, size=11)),
+        list(np.random.normal(loc=10, scale=2, size=11)),
+        list(np.random.normal(loc=10, scale=2, size=11)),
+        list(np.random.normal(loc=13, scale=2, size=11)),
+        list(np.random.normal(loc=10, scale=2, size=11)),
+        list(np.random.normal(loc=10, scale=2, size=11))])
+        
+# Define list variable for groups means
+x_bar = []
+
+# Define list variable for groups standard deviation
+s = [] 
+
+# Get and append groups means and standard deviations
+for group in x:
+    x_bar.append(group.mean())
+    s.append(np.std(group))
+    
+# Plot x-bar and s charts
+fig, axs = plt.subplots(2,figsize=(15,15))
+
+# x-bar chart
+axs[0].plot(x_bar, linestyle='-', marker='o', color='black')
+axs[0].axhline((statistics.mean(x_bar)+0.927*statistics.mean(s)), color='red', linestyle='dashed')
+axs[0].axhline((statistics.mean(x_bar)-0.927*statistics.mean(s)), color='red', linestyle='dashed')
+axs[0].axhline((statistics.mean(x_bar)), color='blue')
+axs[0].set_title("x-bar Chart")
+axs[0].set(xlabel='Group', ylabel='Mean')
+
+# s chart
+axs[1].plot(s, linestyle='-', marker='o', color='black')
+axs[1].axhline((1.649*statistics.mean(s)), color='red', linestyle='dashed')
+axs[1].axhline((0.321*statistics.mean(s)), color='red', linestyle='dashed')
+axs[1].axhline((statistics.mean(s)), color='blue')
+axs[1].set_title("S Chart")
+axs[1].set(xlabel='Group', ylabel='Range')
+
+# Validate points out of control limits for x-bar chart
+i = 0
+control = True
+for group in x_bar:
+    if group > statistics.mean(x_bar)+0.927*statistics.mean(s) or group < statistics.mean(x_bar)-0.927*statistics.mean(s):
+        print("Group", i, "out of mean control limits!")
+        control = False
+    i += 1
+if control == True:
+    print("All points within control limits.")
+    
+# Validate points out of control limits for s chart
+i = 0
+control = True
+for group in s:
+    if group > 1.649*statistics.mean(s) or group < 0.321*statistics.mean(s):
+        print("Group", i, "out of standard deviation cotrol limits!")
+        control = False
+    i += 1
+if control == True:
+    print("All points within control limits.")
