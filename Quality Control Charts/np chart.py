696b9febcea007efa7964389a9ab1cf728e88231
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-09 16:43:45 -0400

    Create np chart.py

---
+++ Quality Control Charts/np chart.py
@@ -0,0 +1,45 @@
+### np chart ###
+
+# Import required libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statistics
+
+# Set random seed
+np.random.seed(42)
+
+# Set matplotlib style
+plt.style.use('ggplot')
+
+# Create dummy data
+data = {'defects':np.random.randint(1,5,10).tolist(),
+        'group_size':np.repeat(10,10).tolist()}
+
+# Convert data to data frame
+data = pd.DataFrame(data)
+
+# Add 'np' column to data frame
+data['np'] = data['defects']/data['group_size']
+
+# Plot np chart
+plt.figure(figsize=(15,7.5))
+plt.plot(p['p'], linestyle='-', marker='o', color='black')
+plt.axhline(statistics.mean(data['np'])+3*(np.sqrt((statistics.mean(data['np'])*(1-statistics.mean(data['np'])))/statistics.mean(data['group_size']))), color='red', linestyle='dashed')
+plt.axhline(statistics.mean(data['np'])-3*(np.sqrt((statistics.mean(data['np'])*(1-statistics.mean(data['np'])))/statistics.mean(data['group_size']))), color='red', linestyle='dashed')
+plt.axhline(statistics.mean(data['np']), color = 'blue')
+plt.ylim(bottom=0)
+plt.title("np Chart")
+plt.xlabel("Group")
+plt.ylabel("Fraction Defective")
+
+# Validate points out of control limits
+i = 0
+control = True
+for group in data['np']:
+    if group > (statistics.mean(data['np'])+3*(np.sqrt((statistics.mean(data['np'])*(1-statistics.mean(data['np'])))/statistics.mean(data['group_size'])))) or group < (statistics.mean(data['np'])-3*(np.sqrt((statistics.mean(data['np'])*(1-statistics.mean(data['np'])))/statistics.mean(data['group_size'])))):
+        print("Group", i, "out of fraction defective cotrol limits!")
+        control = False
+    i += 1
+if control == True:
+    print("All points within control limits.")
