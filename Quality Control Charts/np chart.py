5d0a74a7ca0750244164fa08ffc9bf1de67cb546
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-09 17:09:44 -0400

    Update np chart.py

--- Quality Control Charts/np chart.py
+++ Quality Control Charts/np chart.py
@@ -27,19 +27,19 @@ plt.figure(figsize=(15,7.5))
 plt.plot(p['p'], linestyle='-', marker='o', color='black')
 plt.axhline(statistics.mean(data['np'])+3*(np.sqrt((statistics.mean(data['np'])*(1-statistics.mean(data['np'])))/statistics.mean(data['group_size']))), color='red', linestyle='dashed')
 plt.axhline(statistics.mean(data['np'])-3*(np.sqrt((statistics.mean(data['np'])*(1-statistics.mean(data['np'])))/statistics.mean(data['group_size']))), color='red', linestyle='dashed')
-plt.axhline(statistics.mean(data['np']), color = 'blue')
+plt.axhline(statistics.mean(data['np']), color='blue')
 plt.ylim(bottom=0)
-plt.title("np Chart")
-plt.xlabel("Group")
-plt.ylabel("Fraction Defective")
+plt.title('np Chart')
+plt.xlabel('Group')
+plt.ylabel('Fraction Defective')
 
 # Validate points out of control limits
 i = 0
 control = True
 for group in data['np']:
     if group > (statistics.mean(data['np'])+3*(np.sqrt((statistics.mean(data['np'])*(1-statistics.mean(data['np'])))/statistics.mean(data['group_size'])))) or group < (statistics.mean(data['np'])-3*(np.sqrt((statistics.mean(data['np'])*(1-statistics.mean(data['np'])))/statistics.mean(data['group_size'])))):
-        print("Group", i, "out of fraction defective cotrol limits!")
+        print('Group', i, 'out of fraction defective cotrol limits!')
         control = False
     i += 1
 if control == True:
-    print("All points within control limits.")
+    print('All points within control limits.')
