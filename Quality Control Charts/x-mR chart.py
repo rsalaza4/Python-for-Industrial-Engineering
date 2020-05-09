10c6990e3aee2a08f9bbb8331b82618e070bc5ad
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-09 16:56:31 -0400

    Create x-mR chart.py

---
+++ Quality Control Charts/x-mR chart.py
@@ -0,0 +1,79 @@
+### x-mR chart ###
+
+# Import required libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statistics
+
+# Set random seed
+np.random.seed(42)
+
+# Set matplotlib style
+plt.style.use('ggplot')
+
+# Create dummy data
+x = pd.Series(np.random.normal(loc=10, scale=2, size=10))
+
+# Define list variable for moving ranges
+MR = [np.nan]
+
+# Create dummy data
+x = pd.Series(np.random.normal(loc=10, scale=2, size=10))
+
+# Define list variable for moving ranges
+MR = [np.nan]
+
+# Get and append moving ranges
+i = 1
+for data in range(1, len(x)):
+    MR.append(abs(x[i] - x[i-1]))
+    i += 1
+
+# Convert list to pandas Series objects    
+MR = pd.Series(MR)
+
+# Concatenate mR Series with and rename columns
+data = pd.concat([x,MR], axis=1).rename(columns={0: "x", 1: "mR"})
+
+# Plot x and mR charts
+fig, axs = plt.subplots(2,figsize=(15,15),sharex=True)
+
+# x chart
+axs[0].plot(data['x'], linestyle='-', marker='o', color='black')
+axs[0].axhline(statistics.mean(data['x']), color='blue')
+axs[0].axhline(statistics.mean(data['x'])+3*statistics.mean(data['mR'][1:len(data['mR'])])/1.128, color = "red", linestyle = "dashed")
+axs[0].axhline(statistics.mean(data['x'])-3*statistics.mean(data['mR'][1:len(data['mR'])])/1.128, color = "red", linestyle = "dashed")
+axs[0].set_title("Individual Chart")
+axs[0].set(xlabel='Unit', ylabel='Value')
+
+# mR chart
+axs[1].plot(data['mR'],linestyle='-', marker='o',color='black')
+axs[1].axhline(statistics.mean(data['mR'][1:len(data['mR'])]),color='blue')
+axs[1].axhline(statistics.mean(data['mR'][1:len(data['mR'])])+3*statistics.mean(data['mR'][1:len(data['mR'])])*0.8525, color = "red", linestyle = "dashed")
+axs[1].axhline(statistics.mean(data['mR'][1:len(data['mR'])])-3*statistics.mean(data['mR'][1:len(data['mR'])])*0.8525, color = "red", linestyle = "dashed")
+axs[1].set_ylim(bottom=0)
+axs[1].set_title("mR Chart")
+axs[1].set(xlabel='Unit', ylabel='Range')
+
+# Validate points out of control limits for x chart
+i = 0
+control = True
+for unit in data['x']:
+    if unit > statistics.mean(data['x'])+3*statistics.mean(data['mR'][1:len(data['mR'])])/1.128 or unit < statistics.mean(data['x'])-3*statistics.mean(data['mR'][1:len(data['mR'])])/1.128:
+        print("Unit", i, "out of cotrol limits!")
+        control = False
+    i += 1
+if control == True:
+    print("All points within control limits.")
+    
+# Validate points out of control limits for mR chart
+i = 0
+control = True
+for unit in data['mR']:
+    if unit > statistics.mean(data['mR'][1:len(data['mR'])])+3*statistics.mean(data['mR'][1:len(data['mR'])])*0.8525 or unit < statistics.mean(data['mR'][1:len(data['mR'])])-3*statistics.mean(data['mR'][1:len(data['mR'])])*0.8525:
+        print("Unit", i, "out of cotrol limits!")
+        control = False
+    i += 1
+if control == True:
+    print("All points within control limits.")
