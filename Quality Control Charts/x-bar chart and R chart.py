bb036cb921151a45f26461fa0a8e1c155fd19871
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-09 20:59:27 -0400

    Update x-bar chart and R chart.py

--- Quality Control Charts/x-bar chart and R chart.py
+++ Quality Control Charts/x-bar chart and R chart.py
@@ -9,9 +9,6 @@ import statistics
 # Set random seed
 np.random.seed(42)
 
-# Set matplotlib style
-plt.style.use('ggplot')
-
 # Create dummy data
 x = np.array([list(np.random.normal(loc=10, scale=2, size=5)),
         list(np.random.normal(loc=10, scale=2, size=5)),
