7afc3afb18953ce7340a29dc12124808be7ddc83
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-11 18:17:50 -0400

    Update p chart.py

--- Quality Control Charts/p chart.py
+++ Quality Control Charts/p chart.py
@@ -1,4 +1,4 @@
-### p chart ###
+### p-chart ###
 
 # Import required libraries
 import numpy as np
@@ -19,7 +19,7 @@ p = pd.DataFrame(p)
 # Add 'p' column to data frame
 p['p'] = p['defects']/p['group_size']
 
-# Plot p chart
+# Plot p-chart
 plt.figure(figsize=(15,7.5))
 plt.plot(p['p'], linestyle='-', marker='o', color='black')
 plt.step(x=range(0,len(p['p'])), y=statistics.mean(p['p'])+3*(np.sqrt((statistics.mean(p['p'])*(1-statistics.mean(p['p'])))/(p['group_size']))), color='red', linestyle='dashed')
