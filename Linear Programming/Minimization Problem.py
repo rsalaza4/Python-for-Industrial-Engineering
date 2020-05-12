4fb26e202bff14d821f5281064ac22f119b9ff39
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-12 00:53:39 -0400

    Update Minimization Problem.py

--- Linear Programming/Minimization Problem.py
+++ Linear Programming/Minimization Problem.py
@@ -1,5 +1,14 @@
 ### Linear Programming - Minimization Problem ###
 
+# Example:
+
+# min z = 10x1 + 15x2 + 25x3
+# s.t.
+# 1x1 + 1x2 + 1x3 >= 1000
+# 1x1 - 2x2 + 0x3 >= 0
+# 0x1 + 0x2 + 1x3 >= 340
+#  y1 ,  y2       >= 0
+
 # Import required libraries
 import numpy as np
 from scipy.optimize import linprog
