b818df9865bd0cddad5000b3a3e4a7da3712e682
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-12 00:22:49 -0400

    Update Linear Programming - Maximization.py

--- Linear Programming - Maximization.py
+++ Linear Programming - Maximization.py
@@ -14,13 +14,15 @@ import numpy as np
 from scipy.optimize import linprog
 
 # Set the inequality constraints matrix
+# Note: the inequality constraints must be in the form of <=
 A = np.array([[1, 0], [2, 3], [1, 1], [-1, 0], [0, -1]])
 
 # Set the inequality constraints vector
 b = np.array([16, 19, 8, 0, 0])
 
 # Set the coefficients of the linear objective function vector
-c = np.array([-5,-7]) # when maximizing, change the signs of the c vector coefficient!
+# Note: when maximizing, change the signs of the c vector coefficient
+c = np.array([-5, -7])
 
 # Solve linear programming probelm
 res = linprog(c, A_ub=A, b_ub=b)
