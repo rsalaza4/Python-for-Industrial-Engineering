be113c83cae8775d4cf2d8f9814505b0581e9567
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-12 00:18:52 -0400

    Update Linear Programming - Maximization.py

--- Linear Programming - Maximization.py
+++ Linear Programming - Maximization.py
@@ -1,3 +1,14 @@
+### Linear Programming - Maximization Problem ###
+
+# Example:
+
+# max Z = 5x1 + 7x2
+# s.t.
+# 1x1 + 0x2 <= 16
+# 2x1 + 3x2 <= 19
+# 1x1 + 1x2 <= 8
+#  x1 ,  x2 >= 0
+
 # Import required libraries
 import numpy as np
 from scipy.optimize import linprog
@@ -15,7 +26,7 @@ c = np.array([-5,-7]) # when maximizing, change the signs of the c vector coeffi
 res = linprog(c, A_ub=A, b_ub=b)
 
 # Print results
-print('Optimal value:', round(res.fun*-1,ndigits=2),
-      '\nx values:',res.x,
+print('Optimal value:', round(res.fun*-1, ndigits=2),
+      '\nx values:', res.x,
       '\nNumber of iterations performed:', res.nit,
       '\nStatus:', res.message)
